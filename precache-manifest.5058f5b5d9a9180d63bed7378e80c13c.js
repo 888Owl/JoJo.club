ec9939baaf827313d7ac1038083f26bacf6d5d8a
Mike Hawk <danny_dangle@gmail.com>
2020-09-10 14:37:47 +0800

    Re written in react

---
+++ precache-manifest.5058f5b5d9a9180d63bed7378e80c13c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ebdb0a1db003b579231899f5895bf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3a9d59a199c72460425",
+    "url": "/static/css/main.b3511b06.chunk.css"
+  },
+  {
+    "revision": "d8779701aa4fc5aed12a",
+    "url": "/static/js/2.c3bb6227.chunk.js"
+  },
+  {
+    "revision": "fec8010374a5996c79e5975a9066a6ad",
+    "url": "/static/js/2.c3bb6227.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3a9d59a199c72460425",
+    "url": "/static/js/main.a8f53c67.chunk.js"
+  },
+  {
+    "revision": "294dee5198faa65affa9",
+    "url": "/static/js/runtime-main.3f435202.js"
+  },
+  {
+    "revision": "fc0aa71f80a81694a040cc5783aabcf5",
+    "url": "/static/media/hours-min.fc0aa71f.png"
+  },
+  {
+    "revision": "dfb5a28ea7b6204922c75f01f3c57dec",
+    "url": "/static/media/incentives-min.dfb5a28e.png"
+  },
+  {
+    "revision": "f8f8ed2a3a85b17f54af57f58856975e",
+    "url": "/static/media/j-101-min.f8f8ed2a.png"
+  },
+  {
+    "revision": "8513a3b7ff5795fb429dedc715fe64e7",
+    "url": "/static/media/j-line-min.8513a3b7.png"
+  },
+  {
+    "revision": "290f6dd7b079de12b918639c328a4ea1",
+    "url": "/static/media/j-mob-101-min.290f6dd7.png"
+  },
+  {
+    "revision": "fe80c07195a4c4579a5c13bfd128d642",
+    "url": "/static/media/store-0-min.fe80c071.png"
+  },
+  {
+    "revision": "40b75026a4cfb90eaeba3307fa4b44f2",
+    "url": "/static/media/store-1-min.40b75026.png"
+  },
+  {
+    "revision": "82a1427df91b0e9061038df34386d29a",
+    "url": "/static/media/store-2-min.82a1427d.png"
+  },
+  {
+    "revision": "4f391b4baa2c9f8fe7580b78e10a7c9d",
+    "url": "/static/media/store-3-min.4f391b4b.png"
+  }
+]);
